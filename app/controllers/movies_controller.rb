bdacc6490129a559be32a7c1491232a0cff49592
EC2 Default User <ec2-user@ip-172-31-44-238.us-east-2.compute.internal>
2018-09-28 03:16:56 +0000

    session test attempt 55

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,15 +22,15 @@ class MoviesController < ApplicationController
         @cur_ratings[rating] = 1
       end
     
-    elsif session[:ratings] and session[:sort_by]
+    elsif session[:ratings] and @cur_sort
       flash.keep
-      redirect_to movies_path(:sort_by => session[:sort_by], :rating => session[:ratings].keys)
-      session[:sort_by] = nil
+      redirect_to movies_path(:sort_by => @cur_sort, :rating => session[:ratings].keys)
+      @cur_sort = nil
       
     # if session has ratings stored  
     elsif session[:ratings]
       @movies = Movie.order(params[:sort_by]).where({rating: session[:ratings].keys})
-      session[:sort_by] = params[:sort_by]
+      @cur_sort = params[:sort_by]
       session[:ratings].keys.each do |rating|
         @cur_ratings[rating] = 1
       end
