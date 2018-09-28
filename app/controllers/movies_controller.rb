51ab4b55cc846dc0b997d8693691ce292c7ed7ca
EC2 Default User <ec2-user@ip-172-31-44-238.us-east-2.compute.internal>
2018-09-28 02:15:26 +0000

    session test attempt 55

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,19 +26,19 @@ class MoviesController < ApplicationController
       
       
         
-    elsif session[:ratings] and session[:sort_by]
-      @movies = Movie.order(session[:sort_by]).where({rating: session[:ratings].keys})
-      session[:ratings].keys.each do |rating|
-        @cur_ratings[rating] = 1
-      end  
+    #elsif session[:ratings] and session[:sort_by]
+      #@movies = Movie.order(session[:sort_by]).where({rating: session[:ratings].keys})
+      #session[:ratings].keys.each do |rating|
+        #@cur_ratings[rating] = 1
+      #end  
       
       
     # if session has ratings stored  
-    #elsif session[:ratings] and session[:sort_by] == nil
-      #@movies = Movie.order(params[:sort_by]).where({rating: session[:ratings].keys})
-      #session[:ratings].keys.each do |rating|
-        #@cur_ratings[rating] = 1
-      #end
+    elsif session[:ratings] and session[:sort_by] == nil
+      @movies = Movie.order(params[:sort_by]).where({rating: session[:ratings].keys})
+      session[:ratings].keys.each do |rating|
+        @cur_ratings[rating] = 1
+      end
       
     
     # if no params passed
